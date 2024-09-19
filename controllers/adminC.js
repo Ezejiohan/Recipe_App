a2bc6717350882485b7537d9db5407a7539d998c
Ezejiohan <ezejiohan@gmail.com>
2024-09-19 15:42:32 +0100

    admin forgot password

--- controllers/adminC.js
+++ controllers/adminC.js
@@ -23,4 +23,28 @@ const createAdmin = asyncWrapper(async (req, res, next) => {
     res.status(201).json({ msg: 'Admin created successfully', admin: newAdmin });
 });
 
-module.exports = { createAdmin }
+const adminForgotPassword = asyncWrapper(async (req, res, next) => {
+    const admin = await Admin.findOne({email: req.body.emal})
+    if (!admin) {
+        return next(createCustomError("Admin not found", 404))
+    }
+    const token = jwt.sign({
+        id: admin._id,
+        email: admin.email
+    }, secretKey)
+
+    const passwordChangeLink = `${req.protocol}://${req.get("host")}/admins/change_password/${admin._id}/${token}`;
+    const message = `Click this link: ${passwordChangeLink} to set a new password`;
+
+    sendEmail({
+        email: admin.email,
+        subject: 'Forget password link',
+        message: message
+    });
+
+    res.status(200).json({
+        message: "Email has sent"
+    });
+});
+
+module.exports = { createAdmin, adminForgotPassword }
