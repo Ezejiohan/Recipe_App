259f95d3c698a4f282ac1025d20f85fd8bf7d7b7
Ezejiohan <ezejiohan@gmail.com>
2024-09-20 03:33:11 +0100

    create User

---
+++ controllers/userC.js
@@ -0,0 +1,26 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const asyncWrapper = require('../middleware/async');
+const {createCustomError} = require('../errors/custom_error');
+
+const createUser = asyncWrapper(async(req, res, next) => {
+    const { firstname, lastname, email, password } = req.body;
+    const userExist = await User.findOne({email})
+    if (userExist) {
+        return next(createCustomError("User already exist", 403))
+    }
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
+
+    const newUser = await User.create({
+        firstname,
+        lastname,
+        email,
+        password: hashPassword
+    })
+
+    res.status(201).json({ msg: 'User created successfully', user: newUser });
+})
+
+module.exports = { createUser }
