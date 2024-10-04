aba3a0643c6a4f987517b09e971d4ac5136b2579
Ezejiohan <ezejiohan@gmail.com>
2024-10-04 12:02:07 +0100

    changes on user

--- controllers/userC.js
+++ controllers/userC.js
@@ -1,19 +1,19 @@
-
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const asyncWrapper = require('../middleware/async');
 const { createCustomError } = require('../errors/custom_error');
+const { fetchUser, registerUser } = require('../repository/user');
 
 const createUser = asyncWrapper(async(req, res, next) => {
     const { firstname, lastname, email, password } = req.body;
-    const userExist = await User.findOne({ email })
+    const userExist = await fetchUser({ email })
     if (userExist) {
         return next(createCustomError("User already exist", 403))
     }
     const salt = bcrypt.genSaltSync(10);
     const hashPassword = bcrypt.hashSync(password, salt);
 
-    const newUser = await User.create({
+    const newUser = await registerUser({
         firstname,
         lastname,
         email,
