1a98b2ba0ba6520064b0e14cc32248da371621e7
Ezejiohan <ezejiohan@gmail.com>
2024-10-14 10:43:46 +0100

    user-forgot-password

--- controllers/userC.js
+++ controllers/userC.js
@@ -23,4 +23,28 @@ const createUser = asyncWrapper(async(req, res, next) => {
     res.status(201).json({ msg: 'User created successfully', user: newUser });
 });
 
-module.exports = { createUser }
+const userForgotPassword = asyncWrapper(async (req, res, next) => {
+    const user = await fetchUser({ email: req.body.email });
+    if (!user) {
+        return next(createCustomError('User not found', 404));
+    }
+    const token = jwt.sign({
+        id: user._id,
+        email: user.email
+    }, process.env.TOKEN)
+
+    const passwordChangeLink = `${req.protocol}://${req.get("host")}/userss/change_password/${user._id}/${token}`;
+    const message = `Click this link: ${passwordChangeLink} to set a new password`;
+
+    sendEmail({
+        email: user.email,
+        subject: 'Forget password link',
+        message: message
+    });
+
+    res.status(200).json({
+        message: "Email has been sent"
+    });
+});
+
+module.exports = { createUser, userForgotPassword }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createUser } = require('../controllers/userC');
+const { createUser, userForgotPassword } = require('../controllers/userC');
 
 const userRoute = express.Router();
 
 userRoute.post('/register', (createUser));
+userRoute.post('/userForgotPassword', (userForgotPassword));
 
 module.exports = { userRoute };
